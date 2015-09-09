cocos2d/cocos2d-x

support custom action, and rewrite update function
support custom action in js, and developer can rewrite the update function in custom action class.
useage:
// custom action sample code
var customMoveBy = cc.MoveBy.extend({
    ctor:function (duration, deltaPos, deltaY) {
        this._super(duration, deltaPos, deltaY);
    },
    update:function (dt) {
        this._super(dt);

        if (this.getTarget()) { // rand color
            this.getTarget().setColor(cc.color(cc.rand()%255, cc.rand()%255, cc.rand()%255));
        }

    }
});

cocos/2d/CCActionInstant.cpp
@@ -47,7 +47,15 @@ bool ActionInstant::isDone() const
 
 void ActionInstant::step(float dt) {
     CC_UNUSED_PARAM(dt);
-    update(1);
+    float updateDt = 1;
+#if CC_ENABLE_SCRIPT_BINDING
+    if (_scriptType == kScriptTypeJavascript)
+    {
+        if (ScriptEngineManager::sendRefEventToJS(this, kRefUpdate, (void *)&updateDt))
+            return;
+    }
+#endif
+    update(updateDt);
 }
 
 void ActionInstant::update(float time) {

cocos/2d/CCActionInterval.cpp
@@ -37,6 +37,7 @@ THE SOFTWARE.
 #include "base/CCEventCustom.h"
 #include "base/CCEventDispatcher.h"
 #include "platform/CCStdC.h"
+#include "base/CCScriptSupport.h"
 
 NS_CC_BEGIN
 
@@ -105,6 +106,18 @@ bool ActionInterval::initWithDuration(float d)
     return true;
 }
 
+bool ActionInterval::sendUpdateEventToScript(float dt, Ref *ref)
+{
+#if CC_ENABLE_SCRIPT_BINDING
+    if (_scriptType == kScriptTypeJavascript)
+    {
+        if (ScriptEngineManager::sendRefEventToJS(ref, kRefUpdate, (void *)&dt))
+            return true;
+    }
+#endif
+    return false;
+}
+
 bool ActionInterval::isDone() const
 {
     return _elapsed >= _duration;
@@ -122,12 +135,16 @@ void ActionInterval::step(float dt)
         _elapsed += dt;
     }
     
-    this->update(MAX (0,                                  // needed for rewind. elapsed could be negative
-                      MIN(1, _elapsed /
-                          MAX(_duration, FLT_EPSILON)   // division by 0
-                          )
-                      )
-                 );
+    
+    float updateDt = MAX (0,                                  // needed for rewind. elapsed could be negative
+                           MIN(1, _elapsed /
+                               MAX(_duration, FLT_EPSILON)   // division by 0
+                               )
+                           );
+
+    if (sendUpdateEventToScript(updateDt, this)) return;
+    
+    this->update(updateDt);
 }
 
 void ActionInterval::setAmplitudeRate(float amp)
@@ -323,13 +340,15 @@ void Sequence::update(float t)
         if( _last == -1 ) {
             // action[0] was skipped, execute it.
             _actions[0]->startWithTarget(_target);
-            _actions[0]->update(1.0f);
+            if (!(sendUpdateEventToScript(1.0f, _actions[0])))
+                _actions[0]->update(1.0f);
             _actions[0]->stop();
         }
         else if( _last == 0 )
         {
             // switching to action 1. stop action 0.
-            _actions[0]->update(1.0f);
+            if (!(sendUpdateEventToScript(1.0f, _actions[0])))
+                _actions[0]->update(1.0f);
             _actions[0]->stop();
         }
     }
@@ -339,7 +358,8 @@ void Sequence::update(float t)
         // FIXME: Bug. this case doesn't contemplate when _last==-1, found=0 and in "reverse mode"
         // since it will require a hack to know if an action is on reverse mode or not.
         // "step" should be overriden, and the "reverseMode" value propagated to inner Sequences.
-        _actions[1]->update(0);
+        if (!(sendUpdateEventToScript(0, _actions[1])))
+            _actions[1]->update(0);
         _actions[1]->stop();
     }
     // Last action found and it is done.
@@ -353,8 +373,8 @@ void Sequence::update(float t)
     {
         _actions[found]->startWithTarget(_target);
     }
-
-    _actions[found]->update(new_t);
+    if (!(sendUpdateEventToScript(new_t, _actions[found])))
+        _actions[found]->update(new_t);
     _last = found;
 }
 
@@ -436,8 +456,8 @@ void Repeat::update(float dt)
     {
         while (dt > _nextDt && _total < _times)
         {
-
-            _innerAction->update(1.0f);
+            if (!(sendUpdateEventToScript(1.0f, _innerAction)))
+                _innerAction->update(1.0f);
             _total++;
 
             _innerAction->stop();
@@ -456,19 +476,22 @@ void Repeat::update(float dt)
         {
             if (_total == _times)
             {
-                _innerAction->update(1);
+                if (!(sendUpdateEventToScript(1, _innerAction)))
+                    _innerAction->update(1);
                 _innerAction->stop();
             }
             else
             {
                 // issue #390 prevent jerk, use right update
-                _innerAction->update(dt - (_nextDt - _innerAction->getDuration()/_duration));
+                if (!(sendUpdateEventToScript(dt - (_nextDt - _innerAction->getDuration()/_duration), _innerAction)))
+                    _innerAction->update(dt - (_nextDt - _innerAction->getDuration()/_duration));
             }
         }
     }
     else
     {
-        _innerAction->update(fmodf(dt * _times,1.0f));
+        if (!(sendUpdateEventToScript(fmodf(dt * _times,1.0f), _innerAction)))
+            _innerAction->update(fmodf(dt * _times,1.0f));
     }
 }
 
@@ -710,11 +733,13 @@ void Spawn::update(float time)
 {
     if (_one)
     {
-        _one->update(time);
+        if (!(sendUpdateEventToScript(time, _one)))
+            _one->update(time);
     }
     if (_two)
     {
-        _two->update(time);
+        if (!(sendUpdateEventToScript(time, _two)))
+            _two->update(time);
     }
 }
 
@@ -2241,7 +2266,8 @@ void ReverseTime::update(float time)
 {
     if (_other)
     {
-        _other->update(1 - time);
+        if (!(sendUpdateEventToScript(1 - time, _other)))
+            _other->update(1 - time);
     }
 }
 
@@ -2502,7 +2528,8 @@ void TargetedAction::stop()
 
 void TargetedAction::update(float time)
 {
-    _action->update(time);
+    if (!(sendUpdateEventToScript(time, _action)))
+        _action->update(time);
 }
 
 bool TargetedAction::isDone(void) const

cocos/2d/CCActionInterval.h
@@ -112,6 +112,9 @@ CC_CONSTRUCTOR_ACCESS:
 protected:
     float _elapsed;
     bool   _firstTick;
+
+protected:
+    bool sendUpdateEventToScript(float dt, Ref *ref);
 };
 
 /** @class Sequence

cocos2d-cpp/cocos2d-cpp

cocos/2d/CCActionInstant.cpp
@@ -47,7 +47,15 @@ bool ActionInstant::isDone() const
 
 void ActionInstant::step(float dt) {
     CC_UNUSED_PARAM(dt);
-    update(1);
+    float updateDt = 1;
+#if CC_ENABLE_SCRIPT_BINDING
+    if (_scriptType == kScriptTypeJavascript)
+    {
+        if (ScriptEngineManager::sendActionEventToJS(this, kActionUpdate, (void *)&updateDt))
+            return;
+    }
+#endif
+    update(updateDt);
 }
 
 void ActionInstant::update(float time) {

cocos/2d/CCActionInterval.cpp
@@ -37,6 +37,7 @@ THE SOFTWARE.
 #include "base/CCEventCustom.h"
 #include "base/CCEventDispatcher.h"
 #include "platform/CCStdC.h"
+#include "base/CCScriptSupport.h"
 
 NS_CC_BEGIN
 
@@ -105,6 +106,18 @@ bool ActionInterval::initWithDuration(float d)
     return true;
 }
 
+bool ActionInterval::sendUpdateEventToScript(float dt, Action *actionObject)
+{
+#if CC_ENABLE_SCRIPT_BINDING
+    if (_scriptType == kScriptTypeJavascript)
+    {
+        if (ScriptEngineManager::sendActionEventToJS(actionObject, kActionUpdate, (void *)&dt))
+            return true;
+    }
+#endif
+    return false;
+}
+
 bool ActionInterval::isDone() const
 {
     return _elapsed >= _duration;
@@ -122,12 +135,16 @@ void ActionInterval::step(float dt)
         _elapsed += dt;
     }
     
-    this->update(MAX (0,                                  // needed for rewind. elapsed could be negative
-                      MIN(1, _elapsed /
-                          MAX(_duration, FLT_EPSILON)   // division by 0
-                          )
-                      )
-                 );
+    
+    float updateDt = MAX (0,                                  // needed for rewind. elapsed could be negative
+                           MIN(1, _elapsed /
+                               MAX(_duration, FLT_EPSILON)   // division by 0
+                               )
+                           );
+
+    if (sendUpdateEventToScript(updateDt, this)) return;
+    
+    this->update(updateDt);
 }
 
 void ActionInterval::setAmplitudeRate(float amp)
@@ -323,13 +340,15 @@ void Sequence::update(float t)
         if( _last == -1 ) {
             // action[0] was skipped, execute it.
             _actions[0]->startWithTarget(_target);
-            _actions[0]->update(1.0f);
+            if (!(sendUpdateEventToScript(1.0f, _actions[0])))
+                _actions[0]->update(1.0f);
             _actions[0]->stop();
         }
         else if( _last == 0 )
         {
             // switching to action 1. stop action 0.
-            _actions[0]->update(1.0f);
+            if (!(sendUpdateEventToScript(1.0f, _actions[0])))
+                _actions[0]->update(1.0f);
             _actions[0]->stop();
         }
     }
@@ -339,7 +358,8 @@ void Sequence::update(float t)
         // FIXME: Bug. this case doesn't contemplate when _last==-1, found=0 and in "reverse mode"
         // since it will require a hack to know if an action is on reverse mode or not.
         // "step" should be overriden, and the "reverseMode" value propagated to inner Sequences.
-        _actions[1]->update(0);
+        if (!(sendUpdateEventToScript(0, _actions[1])))
+            _actions[1]->update(0);
         _actions[1]->stop();
     }
     // Last action found and it is done.
@@ -353,8 +373,8 @@ void Sequence::update(float t)
     {
         _actions[found]->startWithTarget(_target);
     }
-
-    _actions[found]->update(new_t);
+    if (!(sendUpdateEventToScript(new_t, _actions[found])))
+        _actions[found]->update(new_t);
     _last = found;
 }
 
@@ -436,8 +456,8 @@ void Repeat::update(float dt)
     {
         while (dt > _nextDt && _total < _times)
         {
-
-            _innerAction->update(1.0f);
+            if (!(sendUpdateEventToScript(1.0f, _innerAction)))
+                _innerAction->update(1.0f);
             _total++;
 
             _innerAction->stop();
@@ -456,19 +476,22 @@ void Repeat::update(float dt)
         {
             if (_total == _times)
             {
-                _innerAction->update(1);
+                if (!(sendUpdateEventToScript(1, _innerAction)))
+                    _innerAction->update(1);
                 _innerAction->stop();
             }
             else
             {
                 // issue #390 prevent jerk, use right update
-                _innerAction->update(dt - (_nextDt - _innerAction->getDuration()/_duration));
+                if (!(sendUpdateEventToScript(dt - (_nextDt - _innerAction->getDuration()/_duration), _innerAction)))
+                    _innerAction->update(dt - (_nextDt - _innerAction->getDuration()/_duration));
             }
         }
     }
     else
     {
-        _innerAction->update(fmodf(dt * _times,1.0f));
+        if (!(sendUpdateEventToScript(fmodf(dt * _times,1.0f), _innerAction)))
+            _innerAction->update(fmodf(dt * _times,1.0f));
     }
 }
 
@@ -710,11 +733,13 @@ void Spawn::update(float time)
 {
     if (_one)
     {
-        _one->update(time);
+        if (!(sendUpdateEventToScript(time, _one)))
+            _one->update(time);
     }
     if (_two)
     {
-        _two->update(time);
+        if (!(sendUpdateEventToScript(time, _two)))
+            _two->update(time);
     }
 }
 
@@ -2241,7 +2266,8 @@ void ReverseTime::update(float time)
 {
     if (_other)
     {
-        _other->update(1 - time);
+        if (!(sendUpdateEventToScript(1 - time, _other)))
+            _other->update(1 - time);
     }
 }
 
@@ -2502,7 +2528,8 @@ void TargetedAction::stop()
 
 void TargetedAction::update(float time)
 {
-    _action->update(time);
+    if (!(sendUpdateEventToScript(time, _action)))
+        _action->update(time);
 }
 
 bool TargetedAction::isDone(void) const

cocos/2d/CCActionInterval.h
@@ -112,6 +112,9 @@ CC_CONSTRUCTOR_ACCESS:
 protected:
     float _elapsed;
     bool   _firstTick;
+
+protected:
+    bool sendUpdateEventToScript(float dt, Action *actionObject);
 };
 
 /** @class Sequence