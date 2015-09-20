fusijie/cocos2d-x

Refactory particle system.

cocos/2d/CCParticleSystem.h
@@ -43,43 +43,101 @@ class ParticleBatchNode;
 /** @struct sParticle
 Structure that contains the values of each particle.
 */
-typedef struct sParticle {
-    Vec2     pos;
-    Vec2     startPos;
 
-    Color4F    color;
-    Color4F    deltaColor;
+struct particle_point
+{
+    float x;
+    float y;
+};
 
-    float        size;
-    float        deltaSize;
+class CC_DLL ParticleData
+{
+public:
+    float* posx;
+    float* posy;
+    float* startPosX;
+    float* startPosY;
 
-    float        rotation;
-    float        deltaRotation;
-
-    float        timeToLive;
-
-    unsigned int    atlasIndex;
-
-    /** @struct modeA
-     Mode A: gravity, direction, radial accel, tangential accel.
-     */
-    struct {
-        Vec2        dir;
-        float        radialAccel;
-        float        tangentialAccel;
+    float* colorR;
+    float* colorG;
+    float* colorB;
+    float* colorA;
+    
+    float* deltaColorR;
+    float* deltaColorG;
+    float* deltaColorB;
+    float* deltaColorA;
+    
+    float* size;
+    float* deltaSize;
+    float* rotation;
+    float* deltaRotation;
+    float* timeToLive;
+    unsigned int* atlasIndex;
+    
+    //! Mode A: gravity, direction, radial accel, tangential accel
+    struct{
+        float* dirX;
+        float* dirY;
+        float* radialAccel;
+        float* tangentialAccel;
     } modeA;
-
-    /** @struct modeB
-     Mode B: radius mode.
-     */
-    struct {
-        float        angle;
-        float        degreesPerSecond;
-        float        radius;
-        float        deltaRadius;
+    
+    //! Mode B: radius mode
+    struct{
+        float* angle;
+        float* degreesPerSecond;
+        float* radius;
+        float* deltaRadius;
     } modeB;
+    
+    unsigned int maxCount;
+    ParticleData();
+    bool init(int count);
+    void release();
+    inline unsigned int getMaxCount(){ return maxCount;}
+    
+    inline void swapParticle(int p1,int p2)
+    {
+        posx[p1] = posx[p2];
+        posy[p1] = posy[p2];
+        startPosX[p1] = startPosX[p2];
+        startPosY[p1] = startPosY[p2];
+        
+        colorR[p1] = colorR[p2];
+        colorG[p1] = colorG[p2];
+        colorB[p1] = colorB[p2];
+        colorA[p1] = colorA[p2];
+        
+        deltaColorR[p1] = deltaColorR[p2];
+        deltaColorG[p1] = deltaColorG[p2];
+        deltaColorB[p1] = deltaColorB[p2];
+        deltaColorA[p1] = deltaColorA[p2];
+        
+        size[p1] = size[p2];
+        deltaSize[p1] = deltaSize[p2];
+        
+        rotation[p1] = rotation[p2];
+        deltaRotation[p1] = deltaRotation[p2];
+        
+        timeToLive[p1] = timeToLive[p2];
+        
+        atlasIndex[p1] = atlasIndex[p2];
+        
+        modeA.dirX[p1] = modeA.dirX[p2];
+        modeA.dirY[p1] = modeA.dirY[p2];
+        modeA.radialAccel[p1] = modeA.radialAccel[p2];
+        modeA.tangentialAccel[p1] = modeA.tangentialAccel[p2];
+        
+        modeB.angle[p1] = modeB.angle[p2];
+        modeB.degreesPerSecond[p1] = modeB.degreesPerSecond[p2];
+        modeB.radius[p1] = modeB.radius[p2];
+        modeB.deltaRadius[p1] = modeB.deltaRadius[p2];
+        
+    }
+};
 
-}tParticle;
+
 
 //typedef void (*CC_UPDATE_PARTICLE_IMP)(id, SEL, tParticle*, Vec2);
 
@@ -192,20 +250,9 @@ public:
      * @js NA
      */
     static ParticleSystem* createWithTotalParticles(int numberOfParticles);
-
-    /** Add a particle to the emitter.
-     *
-     * @return True if add success.
-     * @js ctor
-     */
-    bool addParticle();
-    /** Initializes a particle.
-     * 
-     * @param particle A given particle pointer.
-     */
-    void initParticle(tParticle* particle);
-    /** Stop emitting particles. Running particles will continue to run until they die.
-     */
+    
+    void addParticles(int count);
+    
     void stopSystem();
     /** Kill all living particles.
      */
@@ -218,11 +265,8 @@ public:
 
     /** Update the verts position data of particle,
      should be overridden by subclasses. 
-     *
-     * @param particle A certain particle.
-     * @param newPosition A new position.
      */
-    virtual void updateQuadWithParticle(tParticle* particle, const Vec2& newPosition);
+    virtual void updateParticleQuads();
     /** Update the VBO verts buffer which does not use batch node,
      should be overridden by subclasses. */
     virtual void postStep();
@@ -808,9 +852,9 @@ protected:
         /** Variance in degrees for rotatePerSecond. Only available in 'Radius' mode. */
         float rotatePerSecondVar;
     } modeB;
-
-    //! Array of particles
-    tParticle *_particles;
+    
+    //particle data
+    ParticleData _particleData;
 
     //Emitter name
     std::string _configName;
@@ -820,9 +864,6 @@ protected:
 
     //! How many particles can be emitted per second
     float _emitCounter;
-
-    //!  particle idx
-    int _particleIdx;
 
     // Optimization
     //CC_UPDATE_PARTICLE_IMP    updateParticleImp;

cocos2d-cpp/cocos2d-cpp

cocos/2d/CCParticleSystem.h
@@ -43,43 +43,101 @@ class ParticleBatchNode;
 /** @struct sParticle
 Structure that contains the values of each particle.
 */
-typedef struct sParticle {
-    Vec2     pos;
-    Vec2     startPos;
 
-    Color4F    color;
-    Color4F    deltaColor;
+struct particle_point
+{
+    float x;
+    float y;
+};
 
-    float        size;
-    float        deltaSize;
+class CC_DLL ParticleData
+{
+public:
+    float* posx;
+    float* posy;
+    float* startPosX;
+    float* startPosY;
 
-    float        rotation;
-    float        deltaRotation;
-
-    float        timeToLive;
-
-    unsigned int    atlasIndex;
-
-    /** @struct modeA
-     Mode A: gravity, direction, radial accel, tangential accel.
-     */
-    struct {
-        Vec2        dir;
-        float        radialAccel;
-        float        tangentialAccel;
+    float* colorR;
+    float* colorG;
+    float* colorB;
+    float* colorA;
+    
+    float* deltaColorR;
+    float* deltaColorG;
+    float* deltaColorB;
+    float* deltaColorA;
+    
+    float* size;
+    float* deltaSize;
+    float* rotation;
+    float* deltaRotation;
+    float* timeToLive;
+    unsigned int* atlasIndex;
+    
+    //! Mode A: gravity, direction, radial accel, tangential accel
+    struct{
+        float* dirX;
+        float* dirY;
+        float* radialAccel;
+        float* tangentialAccel;
     } modeA;
-
-    /** @struct modeB
-     Mode B: radius mode.
-     */
-    struct {
-        float        angle;
-        float        degreesPerSecond;
-        float        radius;
-        float        deltaRadius;
+    
+    //! Mode B: radius mode
+    struct{
+        float* angle;
+        float* degreesPerSecond;
+        float* radius;
+        float* deltaRadius;
     } modeB;
+    
+    unsigned int maxCount;
+    ParticleData();
+    bool init(int count);
+    void release();
+    inline unsigned int getMaxCount(){ return maxCount;}
+    
+    inline void copyParticle(int p1,int p2)
+    {
+        posx[p1] = posx[p2];
+        posy[p1] = posy[p2];
+        startPosX[p1] = startPosX[p2];
+        startPosY[p1] = startPosY[p2];
+        
+        colorR[p1] = colorR[p2];
+        colorG[p1] = colorG[p2];
+        colorB[p1] = colorB[p2];
+        colorA[p1] = colorA[p2];
+        
+        deltaColorR[p1] = deltaColorR[p2];
+        deltaColorG[p1] = deltaColorG[p2];
+        deltaColorB[p1] = deltaColorB[p2];
+        deltaColorA[p1] = deltaColorA[p2];
+        
+        size[p1] = size[p2];
+        deltaSize[p1] = deltaSize[p2];
+        
+        rotation[p1] = rotation[p2];
+        deltaRotation[p1] = deltaRotation[p2];
+        
+        timeToLive[p1] = timeToLive[p2];
+        
+        atlasIndex[p1] = atlasIndex[p2];
+        
+        modeA.dirX[p1] = modeA.dirX[p2];
+        modeA.dirY[p1] = modeA.dirY[p2];
+        modeA.radialAccel[p1] = modeA.radialAccel[p2];
+        modeA.tangentialAccel[p1] = modeA.tangentialAccel[p2];
+        
+        modeB.angle[p1] = modeB.angle[p2];
+        modeB.degreesPerSecond[p1] = modeB.degreesPerSecond[p2];
+        modeB.radius[p1] = modeB.radius[p2];
+        modeB.deltaRadius[p1] = modeB.deltaRadius[p2];
+        
+    }
+};
 
-}tParticle;
+
 
 //typedef void (*CC_UPDATE_PARTICLE_IMP)(id, SEL, tParticle*, Vec2);
 
@@ -192,20 +250,9 @@ public:
      * @js NA
      */
     static ParticleSystem* createWithTotalParticles(int numberOfParticles);
-
-    /** Add a particle to the emitter.
-     *
-     * @return True if add success.
-     * @js ctor
-     */
-    bool addParticle();
-    /** Initializes a particle.
-     * 
-     * @param particle A given particle pointer.
-     */
-    void initParticle(tParticle* particle);
-    /** Stop emitting particles. Running particles will continue to run until they die.
-     */
+    
+    void addParticles(int count);
+    
     void stopSystem();
     /** Kill all living particles.
      */
@@ -218,11 +265,8 @@ public:
 
     /** Update the verts position data of particle,
      should be overridden by subclasses. 
-     *
-     * @param particle A certain particle.
-     * @param newPosition A new position.
      */
-    virtual void updateQuadWithParticle(tParticle* particle, const Vec2& newPosition);
+    virtual void updateParticleQuads();
     /** Update the VBO verts buffer which does not use batch node,
      should be overridden by subclasses. */
     virtual void postStep();
@@ -808,9 +852,9 @@ protected:
         /** Variance in degrees for rotatePerSecond. Only available in 'Radius' mode. */
         float rotatePerSecondVar;
     } modeB;
-
-    //! Array of particles
-    tParticle *_particles;
+    
+    //particle data
+    ParticleData _particleData;
 
     //Emitter name
     std::string _configName;
@@ -820,9 +864,6 @@ protected:
 
     //! How many particles can be emitted per second
     float _emitCounter;
-
-    //!  particle idx
-    int _particleIdx;
 
     // Optimization
     //CC_UPDATE_PARTICLE_IMP    updateParticleImp;